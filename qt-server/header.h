5df32abdc7f1b55ffdd6b866834907dbeb168094
lambor.cai <471127079@qq.com>
2018-12-21 15:08:54 +0800

    新文件：   Makefile 	新文件：   header.h 	新文件：   images/icon.rc 	新文件：   images/logo.ico 	新文件：   main.cpp 	新文件：   peerinfo.h 	修改：     qt-server.pro 	新文件：   qt-server.pro.autosave 	新文件：   window.h 	../im-qml.pro.user

---
+++ qt-server/header.h
@@ -0,0 +1,54 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+//General Methods
+#include <QCoreApplication>
+#include <QDebug>
+#include <QFile>
+#include <QByteArray>
+#include <QVector>
+#include <QList>
+#include <QMap>
+#include <QMultiHash>
+#include <QString>
+#include <QDateTime>
+#include <QTime>
+#include <QTimer>
+#include <QMutex>
+
+#include <QQLabel>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QTextList>
+#include <QTextCursor>
+#include <QScrollBar>
+#include <QTextListFormat>
+#include <QTextBrowser>
+#include <QPushButton>
+#include <QListWidget>
+#include <QSpinBox>
+#include <QComboBox>
+#include <QGridLayout>
+
+//Notifition Method
+#include <QMessageBox>
+
+//Qt network
+#include <QtNetWork>
+#include <QHostAddress>
+#include <QNetworkConfigurationManager>
+#include <QNetworkConfiguration>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+//Related to Json File operation
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonValue>
+
+#define MaxBufferSize 1024000
+#define DEBUG 1
+
+#endif //end HEADER_H
+
---
+++ qt-server/peerinfo.h
@@ -0,0 +1,39 @@
+#ifndef PEERINFO_H
+#define PEERINFO_H
+
+#include "header.h"
+#include <QObject>
+
+class PeerInfo : public QObject {
+	Q_OBJECT
+
+public:
+	explicit PeerInfo(QObject *parent = nullptr,QString peername = "unknow", 
+			QHostAddress ip = QHostAddress("127.0.0.1"), int port = 0);
+
+	~PeerInfo();
+
+	QString getPeerName();
+	QString getPerrIP();
+	int 	getPeerPort();
+
+	void	setPeerInfo(QString,QHostAddress,int);
+	void	refresh();
+	void	clear();
+
+signals:
+	void peerExpire(PeerInfo *);
+
+public slots:
+	void handleTimeout();
+
+private:
+	QString 	peerName;
+	QHostAddress	peerIP;
+	int		peerPort;
+	bool		state;
+
+	QTimer		*activeTimer;
+
+};
+#endif //PEERINFO_H /END
