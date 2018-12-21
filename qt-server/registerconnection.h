44571fe7bd3512a950b4bb6f92f684a02404f340
lambor.cai <471127079@qq.com>
2018-12-21 15:42:11 +0800

    o

---
+++ qt-server/registerconnection.h
@@ -0,0 +1,79 @@
+/**
+ * 
+ * author : lambor.cai <lamborcai@gmail.com>			
+ *
+ * Copyright (c) 2018-2025 lanbery,NBS Co.
+ *  
+ * 
+ */
+
+#ifndef REGISTERCONNECTION_H
+#define REGISTERCONNECTION_H
+
+#include "header.h"
+#include <QObject>
+
+class RegisterConnection : public QTcpSocket
+{
+	Q_OBJECT
+public:
+	enum RegisterConnectionState {
+		WaitingForGreeting,
+		ReadingGreeting,
+		ReadyForUse
+	};
+
+	enum DataType
+	{
+		GREETING,
+		REGISTER,
+		PEERLIST,
+		UNDEFINED
+	};
+
+	explicit RegisterConnection(QObject *parent = 0);
+	~RegisterConnection();
+
+	QString 	getPeerServerName();
+
+	QHostAddress 	getPeerServerIP();
+	int		getPeerServerPort();
+
+signals:
+	void		newRegisterMsg(RegisterConnection *,QString);
+	void		registerError(RegisterConnection *,QAbstractSocket::SocketError);
+
+protected:
+	void		timerEvent(QTimerEvent *);
+
+public slots:
+	void		processReadyRead();
+	void		sendGreetingMessage();
+	bool		sendPeriodicPeerlistMessage(QString message);
+	void		handleSocketError(QAbstractSocket::SocketError);
+
+private:
+	int		readDataIntoBuffer(int maxSize = MaxBufferSize);
+	int		dataLengthForCurrentDataType();
+	bool		readProtocolHeader();
+	bool		hasEnougthData();
+	void		processData();
+
+	QString		name;
+	QHostAddress	ip;
+	int		port;
+
+	QByteArray	buffer;
+	enum		RegisterConnectionSate state;
+	enum		DataType	currentDateType;
+
+	int		numBytesForCurrentDateType;
+	int		transferTimerId;
+	bool		isGreetingMessageSent;
+	QString		greetingMessage;
+	QString		registerMessage;
+
+};
+
+#endif //REGISTERCONNECTION_H //
+
