51aaaffe2a70113655183139dbcd2dd0e52bd6a7
lamborqq <471127079@qq.com>
2018-12-31 08:28:45 +0800

    修改：     qt-cli/chatdialog.h

--- qt-cli/chatdialog.h
+++ qt-cli/chatdialog.h
@@ -2,13 +2,43 @@
 #define CHATDIALOG_H
 
 #include "header.h"
+#include "cliconnection.h"
 #include <QDialog>
 
 
 class ChatDialog : public QDialog
 {
+    Q_OBJECT
 public:
-    ChatDialog();
+    explicit ChatDialog(QWidget *parent = nullptr,QString username="localhost",
+                        QString peername="unknown",QString peeraddr = "null",
+                        CliConnection *connection = nullptr);
+
+    ~ChatDialog();
+
+    void    readAndDisplayHistory(QList<QString>);
+
+signals:
+    void    dialogExit(CliConnection *);
+
+public slots:
+    void    handleReceivedMessage(QString from,QString message);
+    void    handleToSendMessage();
+    void    handleConnectionError(QAbstractSocket::SocketError);
+    void    handleRejected();
+
+private:
+
+    void    initLayout();
+
+    QLabel          *peerName;
+    QLabel          *peerAddress;
+    QTextEdit       *logChat;
+    QTextLine       *msgSend;
+    QPushButton     *sendButton;
+
+    QString         userName;
+    CliConnection   *cliSession;
 };
 
 #endif // CHATDIALOG_H
