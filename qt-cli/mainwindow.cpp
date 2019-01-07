41fc6977300269c05f276290b41243c4c527843d
lamborqq <471127079@qq.com>
2019-01-07 15:20:36 +0800

    修改：     qt-cli/mainwindow.cpp

--- qt-cli/mainwindow.cpp
+++ qt-cli/mainwindow.cpp
@@ -120,5 +120,117 @@ void MainWindow::initNetwork(){
             ,this,SLOT(handleNewPeerList(QString &)));
     connect(registerConnection,SIGNAL(error(QAbstractSocket::SocketError))
             ,this,SLOT(QAbstractSocket::SocketError));
+}
+
+void MainWindow::handleRegisterAction() {
+    Logindialog *ld = new Logindialog(this);
+    if(ld->exec() != QDialog::Accepted){
+        QMessageBox::warning(this,tr("Warning"),tr("Chat register canceled!"));
+        return;
+    }
+
+    this->userName = ld->getUserName();
+    this->localServerIP = ld->getLocalServerIP();
+    this->localServerPort = ld->getLocalServerPort();
+    this->remoteServerIP = ld->getRegisterServerIP();
+    this->remoteServerPort = ld->getRegisterServerPort();
+
+    if(DEBUG)
+    {
+        qDebug() << "Register client configuration done!";
+        qDebug() << "Local server name:" << this->userName;
+        qDebug() << "Local server IP address:" << this->localServerIP;
+        qDebug() << "Local server port:" << this->localServerPort;
+    }
+
+    this->initNetwork();
+}
+
+void MainWindow::updateListWidget(){
+    this->peerListWidget->clear();
+
+    for(int i = 0; i < this->peerList.size();i++){
+        QString comment = "";
+        PeerInfo *pi = this->peerList.at(i);
+        QString name = pi->getPeerName();
+
+        if(name == this->userName)
+            comment = "  ( local )";
+        if(this->tempMsgs.contains(name))
+            comment = "  ( new message: " + QString::number(this->tempMsgs[name].size())
+                    +")";
+        QString itemString = pi->getPeerName()
+                + "@" + pi->getPeerIP() + ":"
+                + QString::number(pi->getPeerPort()) + comment;
+
+        this->peerListWidget->addItem(new QListWidgetItem(itemString));
+    }
+}
+
+void MainWindow::handleNewPeerList(QString &peerListString){
+    this->peerList.clear();
+
+    if(peerListString.isEmpty())
+        return;
+
+    QString username,comment;
+    QHostAddress ip;
+    int port;
+
+    QStringList peerInfoList = peerListString.split(";");
+
+    foreach(QString peerInfo,peerInfoList){
+        comment = QString("");
+        QStringList items = peerInfo.split("@");
+        username = items.at(0);
+        QString addr = items.at(1);
+
+        QStringList addrList = addr.split(":");
+
+        QString ipString = addrList.at(0);
+        ip = QHostAddress(ipString);
+        QString portString = addrList.at(1);
+        port = portString.toInt();
+
+        PeerInfo *pinfo = new PeerInfo(this,username,ip,port);
+        this->peerList.push_back(pinfo);
+    }
+
+    this->updateListWidget();
+}
+
+void MainWindow::handlePassiveConnection(CliConnection *connection){
+    if(DEBUG)
+        qDebug() << "New passive chat connection.";
+
+    QString greeting = this->userName + ":" + this->localServerIP.toString()
+            +":" + QString::number(this->localServerPort);
+
+    connection->setGreetingMessage(greeting);
+
+    this->addCliConnection(connection);
+
+    connect(connection,SIGNAL(socketError(CliConnection*,QAbstractSocket::SocketError)),
+            this,SLOT(handleCliConnectionError(CliConnection*,QAbstractSocket::SocketError)));
+
+    connect(connection,SIGNAL(endGreeting(CliConnection*)),this,
+            SLOT(handlePassiveNewCli(CliConnection *)));
+}
+
+//激活链接
+void MainWindow::handleActiveConnection(){
+    if(DEBUG)
+        qDebug() << "New active chat connection.";
+
+    QListWidgetItem *item = this->peerListWidget->currentItem();
+    if(!item){
+        QMessageBox::warning(this,tr("Warning"),tr("Please select one peer."));
+
+        if(DEBUG)
+            qDebug() << "Select no peer to connect.";
+        return;
+    }
+
+    QString connInfo = item->text();
 
 }
